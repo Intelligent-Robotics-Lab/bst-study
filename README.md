7d473cd12aee5e64c583c7e2a67d63b11b828b6e
Pourya Shahverdi <shahverdi@oakland.edu>
2026-05-07 19:17:54 -0400

    Removed the cached gitignore and added __pycache__ to the gitignore

--- README.md
+++
@@ -1,2 +0,0 @@
-# bst-study
-Study 1: Perception of the robot's challenging and supportive behaviors during the BST study.
--- main.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-import logic.bst as bst
-
-async def main():
-    await bst.BST()
-
-if __name__ == "__main__":
-    asyncio.run(main())
